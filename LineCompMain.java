688de99eb1793359df85361e16fa4b0bf5c2a21a
Poonam0107 <120553133+Poonam0107@users.noreply.github.com>
2023-02-06 13:08:05 +0530

    main

---
+++ LineCompMain.java
@@ -0,0 +1,16 @@
+package com.linecomparison;
+
+public class LineCompMain {
+
+	public static void main(String[] args) {
+		
+		//To creating object
+		LineComparison linecomp = new LineComparison();
+		
+		//calling method
+		linecomp.lineLength();
+		linecomp.Comparing();
+
+	}
+
+}
