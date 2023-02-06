f4b3febdf786e62271fe3687b68482a8ed227f00
Poonam Mane <manepoonam001@gmail.com>
2023-02-06 12:48:55 +0530

    Welcome

---
+++ LineComparison_UC1.java
@@ -0,0 +1,20 @@
+package Day3Assignment;
+
+public class LineComparison_UC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program ");
+		int x1= 5;
+		int x2= 4;
+		int y1= 3;
+		int y2= 8;
+		double
+		lengthofLine1 = Math.sqrt(((x2 - x1)^2)+((y2 - y1)^2));
+		System.out.println("This points x1="+x1);
+		System.out.println("This points x2="+x2);
+		System.out.println("This points y1="+y1);
+		System.out.println("This points y2="+y2);
+		System.out.println("Length of Line ="+lengthofLine1);		
+	}
+
+}
